7b842ea26fece0b539a0c6c232e6cec0b9e416c5
taccarlo <tacchella.carlo@gmail.com>
2021-06-08 16:28:07 +0200

    [UP] query of example to my db

--- spring boot project/nationsConnection/src/main/java/com/example/demo/NationsConnectionApplication.java
+++ spring boot project/nationsConnection/src/main/java/com/example/demo/NationsConnectionApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class NationsConnectionApplication{
+public class NationsConnectionApplication implements CommandLineRunner{
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NationsConnectionApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		String sql ="INSERT INTO nation (iso_code, name, capital, population) VALUES ( ?, ?, ?, ?)";
+		int result = jdbcTemplate.update(sql, "GBR", "United Kingdom", "London", 67886004);
+		if(result>0) {
+			System.out.println("A new row has been inserted");
+		}
 	}
 
 }
